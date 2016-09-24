ff04355ba9ff4e10dbbeac0cd9b351f2df092bdf
chenhaoyang <chenhaoyang0529@gmail.com>
2016-09-24 21:10:00 +0800

    init demo

---
+++ demo/demo.js
@@ -0,0 +1,24 @@
+angular.module('demo', ['ng.treeView'])
+  .controller('DemoController', ['$scope',
+    function ($scope) {
+      $scope.searchLabel = '';
+      $scope.pointTree = {
+        treeModel:"pointList",
+
+        iconMode: 'radio',
+
+        selectedKey: 'pointId',
+
+        searchLabel: 'searchLabel',
+
+        nodeId: "pointId",
+
+        nodeLabel: "pointName",
+
+        nodeChildren: "children",
+
+        treeSelectNodeLabelCallBack : function (node) {
+
+        }
+      };
+  }]);
