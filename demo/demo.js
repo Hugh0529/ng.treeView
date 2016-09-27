9b7a185284f6fb43728c66583d58ede6232eebed
chenhaoyang <chenhaoyang0529@gmail.com>
2016-09-27 23:05:41 +0800

    init data

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,21 @@
 angular.module('demo', ['ng.treeView'])
   .controller('DemoController', ['$scope',
     function ($scope) {
+      $scope.pointList = [
+        {
+          'pointId' : 1, 'pointName' : '水果','children' : [
+          {'pointId' : 11, 'pointName' : '苹果','children' : []},
+          {'pointId' : 12, 'pointName' : '香蕉','children' : []}
+        ]},{
+          'pointId' : 2, 'pointName' : '蔬菜','children' : [
+            {'pointId' : 21, 'pointName' : '白菜','children' : []},
+            {'pointId' : 22, 'pointName' : '空心菜','children' : []}
+          ]}
+      ];
+
       $scope.searchLabel = '';
       $scope.pointTree = {
-        treeModel:"pointList",
+        treeModel: 'pointList',
 
         iconMode: 'radio',
 
@@ -18,7 +30,7 @@ angular.module('demo', ['ng.treeView'])
         nodeChildren: 'children',
 
         treeSelectNodeLabelCallBack : function (node) {
-
+          alert(node.pointId);
         }
       };
   }]);
